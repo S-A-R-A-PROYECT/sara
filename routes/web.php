70894314f652907c8867b47418eb961a7bd5683d
rcomunica <julir2772@gmail.com>
2025-09-25 16:37:58 -0500

    FIX | Error en los testing!

--- routes/web.php
+++ routes/web.php
@@ -37,23 +37,23 @@ Route::view('dashboard', 'dashboard')
 
 
 
-// Route::middleware(['auth'])->group(function () {
-//     Route::redirect('settings', 'settings/profile');
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
 
-//     Route::get('settings/profile', Profile::class)->name('settings.profile');
-//     Route::get('settings/password', Password::class)->name('settings.password');
-//     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
-//     Route::get('settings/two-factor', TwoFactor::class)
-//         ->middleware(
-//             when(
-//                 Features::canManageTwoFactorAuthentication()
-//                     && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-//                 ['password.confirm'],
-//                 [],
-//             ),
-//         )
-//         ->name('two-factor.show');
-// });
+    Route::get('settings/two-factor', TwoFactor::class)
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
 
 require __DIR__ . '/auth.php';
